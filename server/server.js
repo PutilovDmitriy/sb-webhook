a30dabf4b9b7a2ea6d859e6b397eb38cc27325d9
Putilov <d.putilov@xsolla.com>
2022-08-30 12:19:14 +0500

    fix port

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const port = 5500
+const port = process.env.PORT || 5000;
 
 const users = [
     { id: '12345678902000', level: 1 },
